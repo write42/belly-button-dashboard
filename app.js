2e906b529a68ce9e7302c16d1ff6e4aca8ff330f
Jade <jadeve.jimenez@yahoo.com>
2021-05-02 07:58:36 -0700

    removed most commented out code

--- app.js
+++ app.js
@@ -63,8 +63,6 @@ function buildCharts(sample_id){
 
         Plotly.newPlot("bubble", bubbleData, bubbleLayout)
 
-        //var yticks = 
-
         var barLayout = {
             title: "Belly Button Bar Chart",
             xaxis:{title: 'OTU'},
@@ -80,28 +78,11 @@ function buildCharts(sample_id){
         }];
 
         Plotly.newPlot("bar", barData, barLayout);
-
-        // bar chart
-        //  var sample_values = sample.sample_values.slice(0,10)
-        // // console.log(sample_values)
-        //  var otu_ids = sample.otu_ids.slice(0,10)
-        //  var otu_labels = sample.otu_labels.slice(0,10)
-        // //console.log(otu_ids)
-        // //console.log(otu_labels)
-        // var start = Object.values(data.metadata)
-        // //console.log(start)
-        // var trace1 = [{
-        //     values: sample_values,
-        //     labels: otu_ids,
-        //     type:'bar'
-        // }];
-        // Plotly.newPlot('bar',trace1)
     });
 }
 
 function init() {
     var selector = d3.select("#selDataset")
-    // call build charts and build metadata
     d3.json(url).then(function(data){  
         var sample_Ids = data.names;
         
@@ -110,37 +91,15 @@ function init() {
             .text(id)
             .property("value", id)
         });
-        //  var sample = data.samples[0]
-        // console.log(sample);
+  
     
-        //  var sample_values = sample.sample_values
-        // // console.log(sample_values)
-        //  var otu_ids = sample.otu_ids
-        //  var otu_labels = sample.otu_labels
-        // //console.log(otu_ids)
-        // // console.log(otu_labels)
-        // var start = Object.values(data.metadata)
- 
-        // console.log(start)
-        
     });
    buildMetaData(940)
    buildCharts(940)
 }
 
 function optionChanged(sample_id){
-    // call build chart and build metadata with the sample_id
     buildMetaData(sample_id)
     buildCharts(sample_id)
 }
-
 init()
-// d3 select [select html element] witht the calss selDatasete
-// append the option element fore each id
-//otu_ids are the labels
-//sample_values are the values
-//otu_labels hovertext
-
-//include sort, exmple below
-//data.sort((a,b) => b.greekSearchResults - a.greekSearchResults)
-//loop through the otu_ids to get them all
